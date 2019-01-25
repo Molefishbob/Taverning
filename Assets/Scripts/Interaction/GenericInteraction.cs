2be5d8fa02242420296ae047fe54644215b5bb98
Finland to Sami <sami.sipari@tuni.fi>
2019-01-25 19:58:55 +0200

    Started tableInteraction and genericinteraction

---
+++ Assets/Scripts/Interaction/GenericInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GenericInteraction : MonoBehaviour
+{
+    public float _actionTime;
+    public Collider2D[] _colliders;
+    private float _timeInteracted;
+    private bool _actionOnGoing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected abstract void Interaction();
+
+    protected abstract void ChangeState(int state);
+}
---
+++ Assets/Scripts/Interaction/TableInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableInteraction : GenericInteraction
+{
+    public enum State
+    {
+        Empty,
+        Full,
+        Fighting
+    }
+
+    public State _currentState;
+    public int _chairCount;
+
+    // Start is called before the first frame update
+     void Start()
+    {
+        
+    }
+
+    protected override void Interaction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void ChangeState(int state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
