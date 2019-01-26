ff9b674449b62cb45711e9e4b122682cdc436a7b
Finland to Sami <sami.sipari@tuni.fi>
2019-01-26 10:30:27 +0200

    Barrels should add stuff to players hands.

---
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public enum hands {
+        Empty = 0,
+        Beer = 1,
+        Mead = 2,
+        Wine = 3
+    }
+    const int InteractableObject = 8;
+    public hands _hands;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.layer == InteractableObject) {
+            other.GetComponent<GenericInteraction>().InteractionStart(this);
+        }
+    }
+}
