803d866f0ef4321ad56634c9d80b27cb0ef867e7
VoidCooper <santeri.sivula@tuni.fi>
2019-01-27 14:51:19 +0200

    ESCTOQUIT and losing

---
+++ Assets/Scripts/ESCTOQUIT.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESCTOQUIT : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Quitted");
+            Application.Quit();
+        }
+    }
+}
